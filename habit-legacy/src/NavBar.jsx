72c8f5f7efa0e400af8791980e6731c2c27eb8fb
Noah Stewart <njstewart22@icloud.com>
2025-03-01 15:24:25 -0800

    Adding profile photo to components

--- habit-legacy/src/NavBar.jsx
+++
@@ -1,4 +0,0 @@
-function NavBar() {
-  return <div className="text-red-500">NavBar</div>;
-}
-export default NavBar;
---
+++ habit-legacy/src/components/ProfilePhoto.jsx
@@ -0,0 +1,4 @@
+function ProfilePhoto({ imgSrc, alt, width }) {
+  return <img className="rounded-full" src={imgSrc} alt={alt} width={width} />;
+}
+export default ProfilePhoto;

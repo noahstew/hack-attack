c0e1d14f54491431246eb6981dc9cf3e17352cab
Noah Stewart <njstewart22@icloud.com>
2025-03-01 11:14:22 -0800

    Forgot to stage these on the last commit lol oops

--- habit-legacy/app/home/page.jsx
+++ habit-legacy/app/home/page.jsx
@@ -1,4 +1,11 @@
+import Image from 'next/image';
+
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      Home
+      <Image src="/discord.png" width={200} height={200} alt="hero" />
+    </div>
+  );
 }
 export default Home;

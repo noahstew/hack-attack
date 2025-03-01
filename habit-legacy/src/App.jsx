798f9601c87094a922c2acda44968a535d9c50c3
MooMan <36575231+Namesnipes@users.noreply.github.com>
2025-03-01 15:45:06 -0800

    App works plz

--- habit-legacy/src/App.jsx
+++ habit-legacy/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import './global.css';
 import Grid from './components/layout/Grid';
-import NavBar from './NavBar';
+// Either import NavBar or remove its usage
+import NavBar from './components/NavBar'; // Add this line if NavBar exists
 
 function App() {
   return (
     <>
       hello
+      {/* If you don't have NavBar component yet, comment this line out */}
       <NavBar />
       <h3>Grid Component</h3>
       <Grid columns="1fr 1fr 1fr" gap="1rem">
@@ -17,3 +19,5 @@ function App() {
     </>
   );
 }
+
+export default App; // This line is crucial and was missing

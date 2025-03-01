2d71a39dc687d3136ad856a41fa613774ed09fb4
MooMan <36575231+Namesnipes@users.noreply.github.com>
2025-03-01 15:38:30 -0800

    Add grid w/ styling

--- habit-legacy/src/App.jsx
+++ habit-legacy/src/App.jsx
@@ -1,14 +1,29 @@
 import './App.css';
+<<<<<<< Updated upstream
 import NavBar from './components/NavBar';
 import Leaderboard from './components/Leaderboard';
 import HabitList from './components/HabitList';
+=======
+import './global.css';
+import Grid from './components/layout/Grid';
+import NavBar from './NavBar';
+>>>>>>> Stashed changes
 
 function App() {
   return (
     <>
       <NavBar />
+<<<<<<< Updated upstream
       <Leaderboard />
       <HabitList />
+=======
+      <h3>Grid Component</h3>
+      <Grid columns="1fr 1fr 1fr" gap="1rem">
+        <div style={{ background: 'var(--color-secondary)', padding: '1rem', borderRadius: 'var(--border-radius)' }}>Grid Item 1</div>
+        <div style={{ background: 'var(--color-secondary)', padding: '1rem', borderRadius: 'var(--border-radius)' }}>Grid Item 2</div>
+        <div style={{ background: 'var(--color-secondary)', padding: '1rem', borderRadius: 'var(--border-radius)' }}>Grid Item 3</div>
+      </Grid>
+>>>>>>> Stashed changes
     </>
   );
 }

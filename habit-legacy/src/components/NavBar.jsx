400975f28efde535ddaa6e735222ffda9be1a83a
Noah Stewart <njstewart22@icloud.com>
2025-03-01 17:33:13 -0800

    Adding favicon

--- habit-legacy/src/components/NavBar.jsx
+++ habit-legacy/src/components/NavBar.jsx
@@ -1,6 +1,27 @@
-import ProfilePhoto from "./ProfilePhoto";
-import ProgressBar from "./ProgressBar";
+import ProfilePhoto from './ProfilePhoto';
+import ProgressBar from './ProgressBar';
 function NavBar() {
-  return <div><img src="photo.png" alt="logo"></img> <h1>Habit Legacy</h1><div>LVL: 40</div><div><ProgressBar current={1100} max={10000}/></div><div>200/290</div><div>Username <ProfilePhoto src="image.png" alt="profilepic"/> </div></div>;
+  return (
+    <div className="flex flex-row justify-between items-center">
+      <div className="flex flex-row items-center">
+        <img src="logo/LogoIcon.png" alt="logo" width={50}></img>{' '}
+        <h1>Habit Legacy</h1>
+      </div>
+      <div className="flex flex-row items-center">
+        <div className="w-96">
+          <ProgressBar current={1100} max={10000} />
+        </div>
+        <div>
+          <div>LVL: 40</div>
+          <div>200/290</div>
+        </div>
+      </div>
+
+      <div className="flex flex-row items-center">
+        <ProfilePhoto imgSrc="icon/eagle.jpeg" alt="profilepic" width={40} />{' '}
+        <p>I have bad habits</p>
+      </div>
+    </div>
+  );
 }
 export default NavBar;

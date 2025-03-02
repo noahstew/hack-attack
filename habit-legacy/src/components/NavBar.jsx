c0d7696bd70734e2be3624a15c7a7e0ba34f097d
minKang01 <smink019@gmail.com>
2025-03-01 17:45:07 -0800

    #	modified:   habit-legacy/src/components/NavBar.jsx

--- habit-legacy/src/components/NavBar.jsx
+++ habit-legacy/src/components/NavBar.jsx
@@ -1,9 +1,5 @@
 import ProfilePhoto from "./ProfilePhoto";
 import ProgressBar from "./ProgressBar";
-function NavBar() { let current = 1100; let max = 10000;
-  return <div><img src="LogoIcon.png" alt="logo"></img> <h1>Habit Legacy</h1><div>LVL: 40</div><div><ProgressBar current={current} max={max}/></div><div>{current}/{max}</div><div>Username <ProfilePhoto src="image.png" alt="profilepic"/> </div></div>;
-import ProfilePhoto from './ProfilePhoto';
-import ProgressBar from './ProgressBar';
 function NavBar() {
   return (
     <div className="flex flex-row justify-between items-center">

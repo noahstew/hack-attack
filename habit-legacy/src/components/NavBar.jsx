045af9bb44f737161c937dfd12f9369fd8fb377b
Noah Stewart <njstewart22@icloud.com>
2025-03-01 18:24:45 -0800

    Styling the NavBar

--- habit-legacy/src/components/NavBar.jsx
+++ habit-legacy/src/components/NavBar.jsx
@@ -1,25 +1,30 @@
-import ProfilePhoto from "./ProfilePhoto";
-import ProgressBar from "./ProgressBar";
+import ProfilePhoto from './ProfilePhoto';
+import ProgressBar from './ProgressBar';
 function NavBar() {
+  const current = 9900;
+  const max = 10000;
+  const level = 40;
   return (
     <div className="flex flex-row justify-between items-center p-4">
-      <div className="flex flex-row items-center">
-        <img src="logo/LogoIcon.png" alt="logo" width={50}></img>{' '}
-        <h1>Habit Legacy</h1>
+      <div className="flex flex-row items-center gap-2">
+        <img src="logo/LogoIcon.png" alt="logo" width={30}></img>{' '}
+        <h1 className="font-semibold">Habit Legacy</h1>
       </div>
-      <div className="flex flex-row items-center">
-        <div className="w-96">
-          <ProgressBar current={1100} max={10000} />
+      <div className="flex flex-col items-center">
+        <div className="flex flex-row items-center gap-2 font-bold text-lg">
+          <p>LVL:</p> <p className="">{level}</p>
         </div>
-        <div>
-          <div>LVL: 40</div>
-          <div>200/290</div>
+        <div className="w-[32rem]">
+          <ProgressBar current={current} max={max} />
+        </div>
+        <div className="text-gray-200 -mb-2 text-sm self-end">
+          {current}/{max}
         </div>
       </div>
 
-      <div className="flex flex-row items-center">
+      <div className="flex flex-row items-center gap-2">
         <ProfilePhoto imgSrc="icon/eagle.jpeg" alt="profilepic" width={40} />{' '}
-        <p>I have bad habits</p>
+        <p>Ben Dover</p>
       </div>
     </div>
   );

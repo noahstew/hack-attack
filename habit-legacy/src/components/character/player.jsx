64466e7c80a7dc54c5ac5a846b388b1be13fd562
harshlall92 <harsh.lall92@gmail.com>
2025-03-01 19:06:43 -0800

    adding player speech bubble

--- habit-legacy/src/components/character/player.jsx
+++ habit-legacy/src/components/character/player.jsx
@@ -1,6 +1,11 @@
 
 export default function Player(){
     return (
-        <img className="absolute bottom-0 left-150 " src = "assets/pixelMascot.png" alt = "Mascot" width="150" height="300"></img>
+        <div>
+            <img className="absolute bottom-0 left-150 " src = "assets/pixelMascot.png" alt = "Mascot" width="150" height="300">
+            </img>
+            <img className= "absolute bottom-0 left-160 " src = "assets/pixelSpeechBubble.png" alt = "textBubble" width="700" >           
+            </img>
+        </div>
     )
 }
